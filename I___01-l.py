4d56e739a31ead653d786da10c60468a82f158a9
pav_lin.desk <pavlentiynsk@gmail.com>
2016-09-07 20:58:07 +0300

    homework 1 (light). Add some correct answers to a dictionary

--- I___01-l.py
+++ I___01-l.py
@@ -14,20 +14,20 @@ questions = ['What is a major version of Python you should choose?',
              'How to call a Zen of Python?'
              ]
 
-answer = ["3",
-         "print",
-         "Guido van Rossum",
-         "str",
-         "import this"
-         ]
+answer = {0: ["3", "3.0", "3.5"],
+          1: ["print", "print()"],
+          2: ["guido van rossum", "guido", "rossum"],
+          3: ["str", "str()"],
+          4: "import this"
+          }
 
 passed_test = 0
 for i in range(len(questions)):
     asked_user = input_function("{} - ".format(questions[i]))
-    if asked_user.lower() == answer[i].lower():
+    if asked_user.lower() in answer[i]:
         print("Correct! ({0} of {1})".format(i+1, len(questions)))
         passed_test += 1
     else:
         print("Wrong! ({0} of {1})".format(i+1, len(questions)))
 
-print("\nFinish! Your answers is correct: -={0}=- ".format(passed_test))
+print("\nFinish! Your answers is correct: -= {0} =- ".format(passed_test))
