192acb5983ad1299e0f1f3983879c8b41fd335fc
pav_lin.desk <pavlentiynsk@gmail.com>
2016-09-08 21:47:35 +0300

    Solution for homework 1 (difficult)

---
+++ lec_01/01hm02-d.py
@@ -0,0 +1,41 @@
+import json
+from datetime import datetime
+
+
+def check_user(login):
+    """Проверяет пользователя на уникальность.
+    Если пользователь существует, возвращает 1, иначе 0"""
+    with open("users.txt", "r") as f:
+        f = f.readlines()
+        for line in f:
+            line = line.split(":")
+            if login == line[0]:
+                return 1
+        return 0
+
+while True:
+    username = input("Please, write your username or type exit for exit -  ")
+    if check_user(username) == 1:
+        print("This username already exists in a users.txt file. Please, try again.")
+        continue
+    elif username.lower() == "exit":
+        break
+    else:
+        str_data = open("ques_ans.json").read()
+        jsdata = json.loads(str_data)
+        passed_test = 0
+        print("Let's start testing...")
+        for i in jsdata["questions"]:
+            asked_user = input("{} - ".format(i["question"]))
+            if asked_user.lower() in i["answers"] and len(asked_user) != 0:
+                print("Correct!")
+                passed_test += 1
+            else:
+                print("Wrong!")
+
+        print("Your result: {0} of {1} done".format(passed_test, len(jsdata["questions"])))
+
+        with open("users.txt", "a") as opened_file:
+            opened_file.write("{login}:{date}:{correct}\n".format(login=username,
+                                date=datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S"), correct=passed_test))
+        break
